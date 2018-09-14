d0018757a9ac2799a24264370a1730bd22911685
alstn0107 <minsu980107@naver.com>
2018-09-14 17:53:00 +0900

    4강쯤끝나고?

---
+++ app/src/main/java/com/example/alstn0107/registeration/RegisterRequest.java
@@ -0,0 +1,31 @@
+package com.example.alstn0107.registeration;
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RegisterRequest extends StringRequest{
+
+    final static private String URL = "http://13.209.24.33/UserRegister.php";
+    private Map<String,String> parameters;
+
+    public RegisterRequest(String userID,String userPassword,String userGender,String userMajor, String userEmail, Response.Listener<String> listener){
+        super(Method.POST,URL,listener,null);
+        parameters = new HashMap<>();
+        parameters.put("userID",userID);
+        parameters.put("userPassword",userPassword);
+        parameters.put("userGender",userGender);
+        parameters.put("userMajor",userMajor);
+        parameters.put("userEmail",userEmail);
+
+    }
+
+    @Override
+    public Map<String,String> getParams(){
+        return parameters;
+    }
+}
+
+
